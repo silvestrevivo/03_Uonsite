e4acc4ebec43070096f801ef9921d3fae1190e85
silvestrevivo <silvestrevivomillan@gmail.com>
2016-09-02 09:05:26 +0200

    New title effect
    Query script for the title - Still adjust responsive

--- assets/js/min/project-min.js
+++ assets/js/min/project-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".banner, h1, h2").addClass("visible")});
+$(document).ready(function(){$(".banner, h2").addClass("visible"),$(".title").find("li").first().fadeTo("slow",1,function i(){$(this).next("li").fadeTo("slow",1,i)})});
--- assets/js/project.js
+++ assets/js/project.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-  $('.banner, h1, h2').addClass('visible');
+  $('.banner, h2').addClass('visible');
+  $('.title').find('li').first().fadeTo('slow', 1 ,  function showNext() {
+    $(this).next('li').fadeTo('slow', 1,  showNext );
+  });
 });
